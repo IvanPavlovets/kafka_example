f2a52d731e96dd65785a3e6123e2f7c49ef9e4f1
IvanPavlovets <ivan150287@gmail.com>
2024-09-20 23:33:01 +0700

    KitchenApp and OredrApp

---
+++ services/kitchen/src/main/java/ru/job4j/kitchen/model/Order.java
@@ -0,0 +1,21 @@
+package ru.job4j.kitchen.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+}
---
+++ services/order/src/main/resources/db/changelog/001_ddl_create_table_orders.sql
@@ -0,0 +1,5 @@
+CREATE TABLE if not EXISTS orders
+(
+    id   serial primary key,
+    name varchar(200)
+    );
